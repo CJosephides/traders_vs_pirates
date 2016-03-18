79ad7ae81317aeb172f6b20c2f85d024326f5ac9
Christos Josephides <cjosephides@gmail.com>
2016-03-18 17:46:24 +0000

    Update demo.
    I updated the demo to show how the set of strategies affects the performance of
    a particular strategy.

--- test_game.py
+++ test_game.py
@@ -7,5 +7,10 @@ pit = core.Pit([strategies.Strategy_Null(),
                 strategies.Strategy_Random(),
                 strategies.Strategy_Random()])
 
+# pit = core.Pit([strategies.Strategy_Null(),
+#                 strategies.Strategy_Null(),
+#                 strategies.Strategy_Null(),
+#                 strategies.Strategy_Random()])
+
 pit.run(n_turns=100, n_games=10)
 viz.plot_pit(pit)
