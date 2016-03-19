9c9909c906d34627f876099dd0ec8cf7038065ed
Christos Josephides <cjosephides@gmail.com>
2016-03-19 11:32:56 +0000

    Demo update.
    Updated and renamed the demo file.

--- demo.py
+++ demo.py
@@ -3,14 +3,19 @@ from tvp import strategies
 from tvp import viz
 
 pit = core.Pit([strategies.Strategy_Null(),
-                strategies.Strategy_Null(),
+                strategies.Strategy_Random(),
                 strategies.Strategy_Random(),
                 strategies.Strategy_Random()])
+
+# pit = core.Pit([strategies.Strategy_Null(),
+#                 strategies.Strategy_Null(),
+#                 strategies.Strategy_Random(),
+#                 strategies.Strategy_Random()])
 
 # pit = core.Pit([strategies.Strategy_Null(),
 #                 strategies.Strategy_Null(),
 #                 strategies.Strategy_Null(),
 #                 strategies.Strategy_Random()])
 
-pit.run(n_turns=100, n_games=10)
+pit.run(n_turns=100, n_games=20)
 viz.plot_pit(pit)
